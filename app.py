427bc1a2262c563de49551db085dee63b84d74ad
Adrian Meza <adrianmeza@Mac.attlocal.net>
2025-07-16 17:52:22 -0700

    Saved all files and added Procfile

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Workout Tracker Home"
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
     
