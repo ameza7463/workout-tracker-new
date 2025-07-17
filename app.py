5f23419e1f530648d66d3002645c74b326321de0
Adrian Meza <adrianmeza@Mac.attlocal.net>
2025-07-16 18:27:13 -0700

    Fix: Updated app.py with correct host and port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,7 +8,7 @@ def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 
     
